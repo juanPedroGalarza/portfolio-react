0fb26944da6fd4454b5133d9e6f02843a69f9dba
juanPedroGalarza <juanpg232@gmail.com>
2022-11-29 17:27:00 -0300

    replace NotFound for Navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { lazy, Suspense } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 import Loading from './components/Loading';
 const MainLayout = lazy(()=>import('./layouts/MainLayout'))
 const Home = lazy(()=>import('./pages/Home'))
-const NotFound = lazy(()=>import('./pages/NotFound'))
 const Proyects = lazy(()=>import('./pages/Proyects'))
 
 function App() {
@@ -16,7 +15,7 @@ function App() {
             <Route index element={<Home />} />
             <Route path='proyects' element={<Proyects />} />
           </Route>
-          <Route path='/*' element={<NotFound />} />
+          <Route path='/*' element={<Navigate to="/"/>} />
         </Routes>
       </Suspense>
     </BrowserRouter>
--- src/pages/NotFound.jsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-    return (
-        <>
-        <h1>Not Found</h1>
-        <p>404</p>
-        </>
-    )
-}

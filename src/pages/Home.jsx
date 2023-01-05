44cd67c1cfe0a3a8c533d13bfb3d6679088d46db
juanPedroGalarza <juanpg232@gmail.com>
2023-01-05 00:00:11 -0300

    contact component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
+import { lazy, Suspense } from "react"
+import Loading from "../components/Loading"
+
+const Contact = lazy(()=>import("../components/Contact"))
+
 export default function Home() {
     return (
         <main className="main">
             <h1>Juan Pedro Galarza</h1>
+            <Suspense fallback={<Loading />}>
+                <Contact />
+            </Suspense>
         </main>
     )
 }

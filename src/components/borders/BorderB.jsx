a32d8660684b4a5aa3ebd16807924632e5c41195
juanPedroGalarza <juanpg232@gmail.com>
2023-01-05 23:04:37 -0300

    borderB active on hover

--- src/components/borders/BorderB.jsx
+++ src/components/borders/BorderB.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react"
 import "../../styles/BorderB.css"
 const BorderB = props => {
   const {
     border
   } = props
+  const [isHover,setIsHover] = useState(false)
   return (
-    <div className="borderb">
+    <div className={`borderb ${isHover?"active":""}`}
+      onMouseEnter={()=>setIsHover(true)}
+      onMouseLeave={()=>setIsHover(false)}
+    >
       <img className="borderb-border" src={border} />
       {props.children}
     </div>

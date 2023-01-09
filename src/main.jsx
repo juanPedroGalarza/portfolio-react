052831a28c25c1b45d75bc66e8243cd0155cb276
juanPedroGalarza <juanpg232@gmail.com>
2023-01-09 17:58:44 -0300

    ThemeProvider and dynamic theme

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,22 @@ import { Provider } from 'react-redux'
 import App from './App'
 import { store } from './features/store'
 import './index.css'
+import { Suspense, lazy } from 'react'
+import Loading from './components/Loading'
+const ThemeProvider = lazy(()=>import('@mui/material/styles/ThemeProvider'))
+import darkTheme from "./features/theme/darkTheme"
+import lightTheme from "./features/theme/lightTheme"
+const themes = {
+  dark: darkTheme,
+  light: lightTheme
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <Suspense fallback={<Loading />}>
+      <ThemeProvider theme={themes[store.getState().theme]}>
+        <App />
+      </ThemeProvider>
+    </Suspense>
   </Provider>
-)
+);

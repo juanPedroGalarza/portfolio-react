a514f908b85c2be7074d5b2d973d640f15fb2d62
juanPedroGalarza <juanpg232@gmail.com>
2023-01-13 18:03:58 -0300

    background MainLayoutBody

--- src/components/StyledComponents/MainLayoutBody.jsx
+++ src/components/StyledComponents/MainLayoutBody.jsx
@@ -2,12 +2,18 @@ import { Box, styled } from "@mui/material";
 import { blueGrey } from "@mui/material/colors";
 
 const BoxBody = styled(Box)(({ theme }) => ({
-  color: blueGrey[100],
-  backgroundColor: theme.palette.background.default,
-  backgroundImage: "url('img/pattern03.png')",
-  backgroundPosition: "50%",
-  backgroundRepeat: "repeat",
-  minHeight: "100vh"
+  color: theme.palette.text.primary,
+  minHeight: "100vh",
+  "&:before": {
+    content: '""',
+    position: "fixed",
+    width: "100%",
+    minHeight: "100vh",
+    backgroundColor: theme.palette.background.default,
+    backgroundImage: "url('img/pattern03.png')",
+    backgroundRepeat: "repeat",
+    zIndex: -1
+  },
 }))
 
 export default function MainLayoutBody(props) {

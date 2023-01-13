2674f37c4812560fd14abf752439db0e63ee1c6e
juanPedroGalarza <juanpg232@gmail.com>
2023-01-13 18:04:44 -0300

    remove sample text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,6 @@ const Contact = lazy(()=>import("../components/Contact"))
 export default function Home() {
     return (
         <main className="main">
-            <h1>Juan Pedro Galarza</h1>
             <Suspense fallback={<Loading />}>
                 <Contact />
             </Suspense>

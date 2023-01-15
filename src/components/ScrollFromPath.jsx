06cb1de0dea66decfbc90e69fc1f46b54e8bce0a
juanPedroGalarza <juanpg232@gmail.com>
2023-01-14 23:09:47 -0300

    bug fix, onLoad page

--- src/components/ScrollFromPath.jsx
+++ src/components/ScrollFromPath.jsx
@@ -4,7 +4,7 @@ import { useLocation } from "react-router-dom"
 export default function ScrollFromPath() {
   const location = useLocation()
   function scrollToSection() {
-    window.scrollTo(0,document.querySelector(location.hash).offsetTop)
+    window.scrollTo(0,document.querySelector(location.hash)?.offsetTop)
   }
   function scrollTop() {
   window.scrollTo(0, 0)

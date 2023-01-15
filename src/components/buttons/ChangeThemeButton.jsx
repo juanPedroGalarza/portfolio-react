3b295e444b848556514c1bfad36363b3594c8ae1
juanPedroGalarza <juanpg232@gmail.com>
2023-01-15 15:25:12 -0300

    ChangeThemeButton ListItem option

--- src/components/buttons/ChangeThemeButton.jsx
+++ src/components/buttons/ChangeThemeButton.jsx
@@ -1,10 +1,10 @@
-import { Icon, IconButton } from "@mui/material";
+import { Icon, IconButton, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
 import { useEffect } from "react";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { changeTheme } from "../../features/theme/themeSlice";
 
-export default function ChangeThemeButton() {
+export default function ChangeThemeButton(props) {
   const dispatch = useDispatch()
   const {themeName} = useSelector(state=>state.theme)
   const [theme, setTheme] = useState(themeName)
@@ -13,6 +13,24 @@ export default function ChangeThemeButton() {
   },[theme])
   const turnTheme = () => {
     themeName === "dark"?setTheme("light"):setTheme("dark")
+  }
+  if (props?.isFor === "List") {
+    return (
+      <ListItem disablePadding>
+        <ListItemButton onClick={turnTheme}
+          dense
+        >
+          <ListItemIcon>
+            <Icon>{themeName === "dark" ?
+              "dark_mode_rounded_icon" :
+              "light_mode_rounded_icon"}</Icon>
+          </ListItemIcon>
+          <ListItemText
+            primary={"Change Theme"}
+             />
+        </ListItemButton>
+      </ListItem>
+    )
   }
   return (
     <IconButton aria-label="change theme"

c5e852d3c1d86a1904006686128444c8c9298761
juanPedroGalarza <juanpg232@gmail.com>
2023-01-16 19:27:52 -0300

    primary color theme

--- src/features/theme/darkTheme.js
+++ src/features/theme/darkTheme.js
@@ -1,8 +1,14 @@
+import { teal } from '@mui/material/colors';
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
   palette: {
     mode: "dark",
+    primary: {
+      main: teal[500],
+      dark: "#00695f",
+      light: "#33ab9f"
+    }
   },
 });
 export default theme
--- src/features/theme/lightTheme.js
+++ src/features/theme/lightTheme.js
@@ -1,4 +1,13 @@
+import { teal } from '@mui/material/colors';
 import { createTheme } from '@mui/material/styles';
 
-const theme = createTheme()
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: teal[800],
+      dark: "#004940",
+      light: "#33877c"
+    }
+  }
+})
 export default theme

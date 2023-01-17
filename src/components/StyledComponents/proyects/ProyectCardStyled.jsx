c6bc5090cfa1d3200e0761c87beac984ec1ec81c
juanPedroGalarza <juanpg232@gmail.com>
2023-01-17 19:52:02 -0300

    ProyectCard & styled update

--- src/components/StyledComponents/proyects/ProyectCardStyled.jsx
+++ src/components/StyledComponents/proyects/ProyectCardStyled.jsx
@@ -1,9 +1,41 @@
 import { Card, styled } from "@mui/material";
 
-const ProyectCardStyled = styled(Card)(({ theme }) => ({
+const ProyectCardStyled = styled(Card)(({ theme, info }) => ({
   position: "relative",
-  width:300,
-  height:400
+  width: 600,
+  height: 400,
+  "& .MuiCardActionArea-root": {
+    height: 280,
+    overflow: "hidden"
+  },
+  "& .MuiCardActions-root": {
+    height: 120
+  },
+  "& .MuiCardContent-root": {
+    width: "100%",
+    height: "100%",
+    overflow: "scroll-y",
+  },
+  "@media (max-width: 1440px)": {
+    width: 400,
+    height: 320,
+    "& .MuiCardActionArea-root": {
+      height: 230,
+    },
+    "& .MuiCardActions-root": {
+      height: 90
+    }
+  },
+  "@media (max-width: 768px)": {
+    width: 320,
+    height: 200,
+    "& .MuiCardActionArea-root": {
+      height: 140,
+    },
+    "& .MuiCardActions-root": {
+      height: 60
+    }
+  }
 }))
 
 export default ProyectCardStyled
--- src/components/proyects/ProyectCard.jsx
+++ src/components/proyects/ProyectCard.jsx
@@ -1,31 +1,52 @@
 import { Button, CardActionArea, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
+import { useState } from "react";
 import { OneBar } from "../StyledComponents/BackgroundBars";
 import ProyectCardStyled from "../StyledComponents/proyects/ProyectCardStyled";
 
 export default function ProyectCard(props) {
-  const {img, proyectName} = props.data
+  const { img, proyectName } = props.data
+  const [showMoreInfo, setShowMoreInfo] = useState(false)
+  const toggleMoreInfo = () => setShowMoreInfo(v => !v)
+
   return (
-    <ProyectCardStyled>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          image={img}
-          heigt="140"
-          alt={proyectName}
-        />
-        <CardContent>
-          <Typography>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo ut necessitatibus voluptates ad iste nemo. Consequuntur qui, laborum nihil natus porro ducimus.
-          </Typography>
-        </CardContent>
+    <ProyectCardStyled
+      raised
+    >
+      <CardActionArea
+        onClick={toggleMoreInfo}
+        info={showMoreInfo.toString()}
+      >
+        {showMoreInfo ?
+          <CardContent>
+            <Typography>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo ut necessitatibus voluptates ad iste nemo. Consequuntur qui, laborum nihil natus porro ducimus.
+            </Typography>
+          </CardContent>
+          : <CardMedia
+            component="img"
+            image={img}
+            heigt="250"
+            alt={proyectName}
+          />
+        }
+        <OneBar color="primary" height="3" />
+        <OneBar color="primary" height="2" second="true" />
       </CardActionArea>
       <CardActions>
         <Button
-          variant="contained"
           color="primary"
+          variant="outlained"
+          onClick={toggleMoreInfo}
         >More Info</Button>
+        <Button
+          color="primary"
+          variant="outlained"
+        >Repository</Button>
+        <Button
+          color="primary"
+          variant="outlained"
+        >View proyect</Button>
       </CardActions>
-      <OneBar color="primary" height="3" />
     </ProyectCardStyled>
   )
 }

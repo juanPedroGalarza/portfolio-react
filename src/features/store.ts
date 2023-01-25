dffe96ab4c6dd5db809ca70b343460b2f450c2d3
juanPedroGalarza <juanpg232@gmail.com>
2023-01-25 12:09:20 -0300

    fix StoreInterface and export

--- src/features/store.ts
+++ src/features/store.ts
@@ -4,14 +4,16 @@ import { Proyect } from "./proyects/proyectsData";
 import proyectsReducer from "./proyects/proyectsSlice";
 import themeReducer from "./theme/themeSlice";
 
-interface Store {
-  theme: string,
+export interface StoreInterface {
+  theme: {
+    themeName: string
+  },
   proyectsData: {
     proyects: Array<Proyect>
   }
 }
 
-export const store: ToolkitStore<Store> = configureStore({
+export const store: ToolkitStore<StoreInterface> = configureStore({
   reducer: {
     theme: themeReducer,
     proyectsData: proyectsReducer

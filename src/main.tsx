2f7eec1b64b0b53ac1e61a62ee6464d3b3f7628a
juanPedroGalarza <juanpg232@gmail.com>
2023-01-25 19:05:11 -0300

    Main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { Suspense, lazy } from 'react'
 import Loading from './components/Loading'
+import React from 'react'
 const ProviderStore = lazy(()=>import('./ProviderStore'))
 const App = lazy(()=>import('./App'))
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.querySelector('#root')).render(
   <Suspense fallback={<Loading />}>
     <ProviderStore>
       <App />

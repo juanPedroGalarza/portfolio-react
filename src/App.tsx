3277a816db74124c8db229608213643d8785761f
juanPedroGalarza <juanpg232@gmail.com>
2023-01-25 19:05:42 -0300

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Theme } from '@mui/material';
+import React from 'react';
 import { lazy, Suspense } from 'react';
 import { useSelector } from 'react-redux';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 import Loading from './components/Loading';
+import { StoreInterface } from './features/store';
 const MainLayout = lazy(()=>import('./layouts/MainLayout'))
 const Home = lazy(()=>import('./pages/Home'))
 const Proyects = lazy(() => import('./pages/Proyects'))
@@ -10,14 +13,18 @@ const ThemeProvider = lazy(()=>import('@mui/material/styles/ThemeProvider'))
 import darkTheme from "./features/theme/darkTheme"
 import lightTheme from "./features/theme/lightTheme"
 
+interface Themes {
+  [key:string]:Theme
+}
+
 
 function App() {
-  const themes = {
+  const themes: Themes = {
     dark: darkTheme,
     light: lightTheme
   }
 
-  const {themeName} = useSelector(state=>state.theme)
+  const {themeName} = useSelector((state:StoreInterface)=>state.theme)
   return (
     <Suspense fallback={<Loading />}>
       <ThemeProvider theme={themes[themeName]}>

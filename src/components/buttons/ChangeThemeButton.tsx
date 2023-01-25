e0c58ada8419da6625e7145df88b2990058d9ae9
juanPedroGalarza <juanpg232@gmail.com>
2023-01-25 12:09:47 -0300

    ChangeThemeButton.tsx

--- src/components/buttons/ChangeThemeButton.tsx
+++ src/components/buttons/ChangeThemeButton.tsx
@@ -1,20 +1,20 @@
+import * as React from "react";
 import { Icon, IconButton, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
-import { useEffect } from "react";
-import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { changeTheme } from "../../features/theme/themeSlice";
+import { StoreInterface } from "../../features/store";
 
-export default function ChangeThemeButton(props) {
+export default function ChangeThemeButton({isFor}:{isFor?:string}) {
   const dispatch = useDispatch()
-  const {themeName} = useSelector(state=>state.theme)
-  const [theme, setTheme] = useState(themeName)
-  useEffect(() => {
+  const {themeName} = useSelector((state:StoreInterface)=>state.theme)
+  const [theme, setTheme] = React.useState(themeName)
+  React.useEffect(() => {
     dispatch(changeTheme(theme))
   },[theme])
   const turnTheme = () => {
     themeName === "dark"?setTheme("light"):setTheme("dark")
   }
-  if (props?.isFor === "List") {
+  if (isFor === "List") {
     return (
       <ListItem disablePadding>
         <ListItemButton onClick={turnTheme}

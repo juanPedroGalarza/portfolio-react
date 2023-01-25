4d61bb88327d2a7ca9fa8654bbb4a659b78f2283
juanPedroGalarza <juanpg232@gmail.com>
2023-01-24 23:05:12 -0300

    ScrollFromPath migrated to typescript

--- src/components/ScrollFromPath.tsx
+++ src/components/ScrollFromPath.tsx
@@ -1,10 +1,16 @@
+import * as React from "react"
 import { useEffect } from "react"
 import { useLocation } from "react-router-dom"
+
+interface ElementOnWindow extends Element {
+  offsetTop: number
+}
 
 export default function ScrollFromPath() {
   const location = useLocation()
   function scrollToSection() {
-    window.scrollTo(0,document.querySelector(location.hash)?.offsetTop)
+    const elementLocated: ElementOnWindow | null = document.querySelector(location.hash)
+    elementLocated && window.scrollTo(0,elementLocated.offsetTop)
   }
   function scrollTop() {
   window.scrollTo(0, 0)

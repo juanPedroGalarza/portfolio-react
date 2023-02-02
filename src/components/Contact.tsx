5aff3a513e76af11bfe2d147a0df917e25c335a4
juanPedroGalarza <juanpg232@gmail.com>
2023-02-02 19:48:02 -0300

    social Networks and more Contacts

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,10 +1,80 @@
 import { Box, Button, Grid, TextField, Typography } from "@mui/material"
 import React from "react"
+import { useSelector } from "react-redux"
+import { StoreInterface } from "../features/store"
 import ContactStyled from "./StyledComponents/ContactStyled"
 
+interface NetworkItem {
+  name: string,
+  image: string,
+  href: string,
+  sub: string
+}
+
 export default function Contact() {
+  const {themeName} = useSelector((state:StoreInterface)=>state.theme)
+
+  function printNetwork(item:NetworkItem) {
+    return (
+      <Grid item xs={12} sm={6}>
+        <Box sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center"
+        }}>
+          <Typography>{item.name}</Typography>
+          <Button
+            startIcon={<img src={item.image} alt={item.name}
+              style={{
+                height: 50, width: 50,
+                filter: themeName === "dark" ? "invert(100%)" : null
+              }} />}
+            href={item.href}
+            color="secondary"
+          >
+          {item.sub}
+          </Button>
+        </Box>
+      </Grid>
+    )
+  }
+
+  const listNetwork: Array<NetworkItem> = [
+    {
+      name: "GitHub",
+      image: "https://i.postimg.cc/MK7C7W9w/logo-github.png",
+      href: "https://github.com/juanPedroGalarza",
+      sub: "juanPedroGalarza"
+    },
+    {
+      name: "Linkedin",
+      image: "https://i.postimg.cc/sfhd2PSv/logo-linkedin.png",
+      href: "https://www.linkedin.com/in/juan-pedro-galarza",
+      sub: "juan pedro galarza"
+    },
+    {
+      name: "Phone",
+      image: "https://i.postimg.cc/FzHYy9SH/logo-phone.png",
+      href: "tel:+5403425588401",
+      sub: "+5403425588401"
+    },
+    {
+      name: "Mail",
+      image: "https://i.postimg.cc/J7ybZ4c2/logo-mail.png",
+      href: "mailto:juanpg232@gmail.com",
+      sub: "juanpg232@gmail.com"
+    },
+  ]
+
+
   return (
-    <Box>
+    <Box sx={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "stretch",
+      flexDirection: "column"
+    }}>
       <Typography variant="h4" paragraph>Contact me</Typography>
       <ContactStyled
         id="contact"
@@ -48,6 +118,13 @@ export default function Contact() {
         <Button type="submit" color="success" variant="outlined"
         >Send</Button>
       </ContactStyled>
+      <Grid container spacing={5} wrap="wrap"
+        sx={{
+        marginTop: "5rem",
+        marginBottom: "5rem"
+      }}>
+        {listNetwork.map(printNetwork)}
+      </Grid>
     </Box>
   )
 }
